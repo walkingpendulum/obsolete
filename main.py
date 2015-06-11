79b7cea14898ebfd76755134f32aa3d0e3235e5d
Oleg Yunin <core.safecrlf>
2015-06-12 00:41:14 +0300

    patch #5
    added '--logs' command to command-line interface

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from Planet import Planet
 
 from BasicStrategy import BasicAnt, BasicBase
-from FireAnt import FireAnt, FireAntBase
-
 
 def dump(planet, path):
     with open(path, mode='a') as f:
@@ -14,22 +12,20 @@ def dump(planet, path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--size', type=str, dest='size', default='50 30', help='Size of map, pair of integer as "x_size y_size". By default equals to "50 30"')
-    parser.add_argument('-d', '--delay', type=float, dest='delay', default=0.001, help='Delay between turns in second. By deafult equals to 0.3 sec.')
+    parser.add_argument('-d', '--delay', type=float, dest='delay', default=0.01, help='Delay between turns in second. By deafult equals to 0.3 sec.')
+    parser.add_argument('--logs', action='store_true', dest='logs_flag', help='Enable log gathering.')
     args = parser.parse_args()
     log_name = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
 
-    FirstAntClass, FirstBaseClass = FireAnt, FireAntBase
+    FirstAntClass, FirstBaseClass = BasicAnt, BasicBase
     SecondAntClass, SecondBaseClass = BasicAnt, BasicBase
     Earth = Planet(size=map(int, args.size.split()),
                    AntClass1=FirstAntClass, BaseClass1=FirstBaseClass,
                    AntClass2=SecondAntClass, BaseClass2=SecondBaseClass)
-#    while True:
-    for i in range(10000):
+    while True:
         Earth.advance()
-        if not (i % 1000):
-            print Earth
-#        dump(Earth, log_name)
-#        print Earth
-#        sleep(args.delay)
-
-    print Earth
+        print Earth
+        print len(Earth.Base1.catalog), len(Earth.Base2.catalog)
+        if args.logs_flag:
+            dump(Earth, log_name)
+        sleep(args.delay)
