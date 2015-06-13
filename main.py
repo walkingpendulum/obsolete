2d3a1aa49f1f4e533cc5ed68c360ca715eaff7a2
Oleg Yunin <core.safecrlf>
2015-06-14 02:02:46 +0300

    pass

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+# -*- coding: utf-8 -*-
+
 import argparse
 from time import sleep
 from datetime import datetime
-from Planet import Planet
 
+from planet import Planet
 from BasicStrategy import BasicAnt, BasicBase
 
-def dump(planet, path):
-    with open(path, mode='a') as f:
+
+def dump(planet, filename):
+    ''' Сбрасывает текущее состояние поля str(planet) в файл filename.'''
+    with open(filename, mode='a') as f:
         f.write(str(planet).replace('\n', '$') + '\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--size', type=str, dest='size', default='50 30', help='Size of map, pair of integer as "x_size y_size". By default equals to "50 30"')
+    parser.add_argument('-s', '--size', type=str, dest='size', default='50 21', help='Size of map, pair of integer as "x_size y_size". By default equals to "50 21"')
     parser.add_argument('-d', '--delay', type=float, dest='delay', default=0.5, help='Delay between turns in second. By deafult equals to 0.3 sec.')
     parser.add_argument('--logs', action='store_true', dest='logs_flag', help='Enable log gathering.')
     args = parser.parse_args()
