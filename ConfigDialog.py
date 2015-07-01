005a2c4c7b5c23a9e8f08beafd12dae73f4489a0
Artom-Kozincev <s17b2_kozincev@179.ru>
2015-07-01 04:33:48 +0300

    Now there is graphical settings window! TODO: return console settings.

--- ConfigDialog.py
+++ ConfigDialog.py
@@ -3,8 +3,9 @@ from Tkinter import *
 from loader import Strategy, Loader
 
 class ConfigDialog:
-    def __init__(self, result, noindex=[]):
+    def __init__(self, result, config):
         self.result = result
+        self.config = config
         self.lo = Loader()
         top = self.top = Tk()
         Label(top, text='Choose strategies (just close this window when ready):').grid(row=0, columnspan=2)
@@ -21,6 +22,26 @@ class ConfigDialog:
         self.list = Listbox(top)
         self.list.grid(row=1, column=1, rowspan=2, sticky=NS)
         Button(top, text='Delete', command=lambda lb=self.list: lb.delete(ANCHOR)).grid(row=3, column=1)
+        self.theme = StringVar()
+        Label(top, text='Choose theme:').grid(row=4, column=0, sticky=W)
+        OptionMenu(top, self.theme, *[filename[:-3] for filename in os.listdir('themes')]).grid(row=4, column=1, sticky=EW)
+        Label(top, text='Field width:').grid(row=5, column=0, sticky=W)
+        self.width = Spinbox(top, from_=10, to=300)
+        self.width.delete(0, END)
+        self.width.insert(0, 50)
+        self.width.grid(row=5, column=1, sticky=EW)
+        Label(top, text='Field height:').grid(row=6, column=0, sticky=W)
+        self.height = Spinbox(top, from_=10, to=300)
+        self.height.grid(row=6, column=1, sticky=EW)
+        self.height.delete(0, END)
+        self.height.insert(0, 21)
+        Label(top, text='Step delay (in ms):').grid(row=7, column=0, sticky=W)
+        self.delay = Spinbox(top, from_=100, to=5000)
+        self.delay.delete(0, END)
+        self.delay.insert(0, 500)
+        self.delay.grid(row=7, column=1, sticky=EW)  
+        self.enable_logs = IntVar()
+        Checkbutton(top, text='Enable logs', variable=self.enable_logs).grid(row=8, column=0, columnspan=2, sticky=W)
         top.protocol('WM_DELETE_WINDOW', self.ok)
         top.mainloop()
         
@@ -37,4 +58,9 @@ class ConfigDialog:
         names = [o.name + ' ' + o.version for o in self.index]
         for name in list(self.list.get(0, END)):
             self.result.append(self.index[names.index(name)])
+        self.config['theme'] = (self.theme.get() if self.theme.get() else 'constructor')
+        self.config['width'] = (int(self.width.get()) if self.width.get().isdigit() else 50)
+        self.config['height'] = (int(self.height.get()) if self.height.get().isdigit() else 21)
+        self.config['delay'] = (int(self.delay.get()) if self.delay.get().isdigit() else 500)
+        self.config['enable_logs'] = bool(self.enable_logs.get())
         self.top.destroy()
--- main.py
+++ main.py
@@ -9,27 +9,30 @@ from ConfigDialog import ConfigDialog
 from loader import Strategy
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--size', type=str, dest='size', default='50 21', help='Size of map, pair of integer as "x_size y_size". By default equals to "50 21"')
-    parser.add_argument('-d', '--delay', type=float, dest='delay', default=500, help='Delay between turns in ms.')
-    parser.add_argument('--logs', action='store_true', dest='logs_flag', help='Enable logs gathering.')
-    parser.add_argument('-t', '--theme', type=str, dest='theme', default='constructor', help='Theme for graphical interface.')
+    # # TODO: Return console interface.
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-s', '--size', type=str, dest='size', default='50 21', help='Size of map, pair of integer as "x_size y_size". By default equals to "50 21"')
+    # parser.add_argument('-d', '--delay', type=float, dest='delay', default=500, help='Delay between turns in ms.')
+    # parser.add_argument('--logs', action='store_true', dest='logs_flag', help='Enable logs gathering.')
+    # parser.add_argument('-t', '--theme', type=str, dest='theme', default='constructor', help='Theme for graphical interface.')
 
-    args = parser.parse_args()
-    log_name = datetime.now().strftime("%y-%m-%d-%H-%M-%S") if args.logs_flag else None
-
-    # team1 = Team(AntClass=BasicAnt, BaseClass=BasicBase, team_id=1)
-    # team2 = Team(AntClass=BasicAnt, BaseClass=BasicBase, team_id=2)
+    # args = parser.parse_args()
+    
+    # # Not working anyway.
+    # log_name = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    log_name = None
+    
     teams = set()
     strategies = []
-    ConfigDialog(strategies)
+    config = dict()
+    ConfigDialog(strategies, config)
     for i in range(len(strategies)):
         teams.update({Team(AntClass=strategies[i].AntClass, BaseClass=strategies[i].BaseClass, team_id=i + 1)})
     
-    AntWarsGame = gameController(size=tuple(map(int, args.size.split())),
-                                 delay=args.delay,
-                                 log_name=log_name,
-                                 themeStr=args.theme
+    AntWarsGame = gameController(size=(config['width'], config['height']),
+                                 delay=config['delay'],
+                                 log_name=(log_name if config['enable_logs'] else None),
+                                 themeStr=config['theme']
                                  )
 
     AntWarsGame.Init(teams=teams)
