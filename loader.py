30ae7d129bd04966ab2e4a202f9f7ef306ebee92
walkingpendulum <oayunin@gmail.com>
2016-04-04 20:48:22 +0300

    mv names to paths
    pros:
    * bash autocomplete for paths
    
    cons:
    * names a bit longer than before
    * gui fix needed

--- loader.py
+++ loader.py
@@ -1,31 +1,26 @@
 import importlib
-
+import os
 
 class Strategy:
-    def __init__(self, name, description, version, BaseClass, AntClass):
+    def __init__(self, name, description, BaseClass, AntClass):
         self.name = name
         self.description = description
-        self.version = version
         self.BaseClass = BaseClass
         self.AntClass = AntClass
 
 
 class Loader:
-    def __init__(self):
-        pass
-
-    def loadStrategy(self, strategyName):
-        strategy_module = importlib.import_module('strategies.%s' % strategyName)
+    def loadStrategy(self, path):
+        normalized_name = os.path.split(path)[-1].split('.')[0]     # dir/name.py -> name
+        strategy_module = importlib.import_module('strategies.%s' % normalized_name)
         manifest = strategy_module.MANIFEST
 
         if not isinstance(manifest, dict) or 'BaseClass' not in manifest or 'AntClass' not in manifest:
             raise ValueError('Incorrect strategy specified.')
         if 'description' not in manifest:
             manifest['description'] = '...'
-        if 'version' not in manifest:
-            manifest['version'] = '1.0'
-        manifest['name'] = strategyName
+        manifest['name'] = normalized_name
         return Strategy(**manifest)
 
-    def loadStrategies(self, strategyNames):
-        return [self.loadStrategy(name) for name in strategyNames]
+    def loadStrategies(self, paths):
+        return [self.loadStrategy(path) for path in paths]
