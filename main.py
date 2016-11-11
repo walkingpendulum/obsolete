ab685459cce32b8d1fc69e6b6f2754a75078e65e
Oleg Yunin <oayunin@gmail.com>
2016-11-12 00:00:55 +0300

    refaaactor

--- main.py
+++ main.py
@@ -22,19 +22,18 @@ class iterate_with_ETA_output(object):
                 raise NotImplementedError("Iterable has no __len__, specify 'total' argument")
             total = len(iterable)
         self.total = total
-        self.start_time = time.time()
+        self.start_time = int(time.time())
         self.iterator = self._gen(iterable)
+        if isinstance(stream, str):
+            self.stream = open(stream, 'w')
         self.stream = stream if stream else sys.stdout
-        self.need_to_close = bool(stream)
-
-        time.sleep(1e-3)
+        self.not_stdout = bool(stream)
 
     def print_to_stream(self, msg):
-        self.stream.write(msg)
-        self.stream.flush()
+        print(msg, end='', file=self.stream)
 
     def close_stream_if_needed(self):
-        if self.need_to_close:
+        if self.not_stdout:
             self.stream.close()
 
     def __iter__(self):
@@ -46,19 +45,32 @@ class iterate_with_ETA_output(object):
             rate = (1 + ind) / self.total
             percentage = 100 * rate
 
-            passed_seconds = int(time.time() - self.start_time)
+            _passed_seconds = time.time() - self.start_time
+            passed_seconds = int(_passed_seconds)
             passed_m, passed_s = divmod(passed_seconds, 60)
-            eta_seconds = int(passed_seconds * ((1 - rate) / rate))
+
+            eta_seconds = int(_passed_seconds * ((1 - rate) / rate))
             eta_m, eta_s = divmod(eta_seconds, 60)
 
-            passed_str = "%02d:%02ds" % (passed_m, passed_s)
-            eta_str = "%02d:%02ds" % (eta_m, eta_s)
-            _output_str = '{:.2f}%, ETA {}, passed {}'.format(percentage, eta_str, passed_str)
-            output_str = '\r' + '{:<90}'.format(_output_str)
+            _total_seconds = _passed_seconds / rate
+            total_seconds = int(_total_seconds)
+            total_m, total_s = divmod(total_seconds, 60)
+
+            str_template = "%02d:%02ds"
+            passed_str = str_template % (passed_m, passed_s)
+            eta_str = str_template % (eta_m, eta_s)
+            total_str = str_template % (total_m, total_s)
+
+            _output_str = '{:.2f}%, ETA {}, passed {}, total ~ {}'.format(percentage, eta_str, passed_str, total_str)
+            output_str = '\r' + '{:<60}'.format(_output_str)
 
             self.print_to_stream(output_str)
             return value
-        except (StopIteration, KeyboardInterrupt):
+        except StopIteration:
             self.print_to_stream('\n')  # перевод строки
             self.close_stream_if_needed()
             raise
+        except KeyboardInterrupt:
+            self.print_to_stream('\n')  # перевод строки
+            self.close_stream_if_needed()
+            raise StopIteration
